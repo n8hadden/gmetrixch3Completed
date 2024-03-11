4eb5bda615904a9580971757f9341bfca618ad7e
n8hadden <n8hadden888@gmail.com>
2024-03-11 14:19:30 -0700

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+﻿const salesTaxRate = .075;
+incMsg = "This site is under construction and will be fully functional soon.";
+function product(item, size, price) {
+    this.item = item;
+    this.size = size;
+    this.price = price;
+}
